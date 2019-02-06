41576b295028c310c7e2ade75745a2a13bc0d1c6
Lewis Turner <lewis.turner@lewiss-air.home>
2019-02-06 19:36:01 +0000

    created a nice little chat app

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,48 @@ import React, { Component } from 'react'
 import io from 'socket.io-client'
 import { withStyles } from '@material-ui/core/styles'
 
+import Avatar from '@material-ui/core/Avatar'
+import Blue from '@material-ui/core/colors/blue'
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Divider from '@material-ui/core/Divider'
+import Green from '@material-ui/core/colors/green'
+import Grey from '@material-ui/core/colors/grey'
+import Paper from '@material-ui/core/Paper'
+import Purple from '@material-ui/core/colors/purple'
+import Red from '@material-ui/core/colors/red'
 import TextField from '@material-ui/core/TextField'
 
 const socket = io.connect()
+const colors = [Red, Green, Blue, Purple]
 const styles = theme => ({
   root: {
-    margin: theme.spacing.unit * 3
+    height: '100%',
+    padding: theme.spacing.unit * 3,
+    backgroundColor: Grey[300]
   },
   textfieldContainer: {
-    display: 'flex'
+    display: 'flex',
+    justifyContent: 'space-between'
   },
   textfield: {
+    backgroundColor: 'white',
+    marginRight: theme.spacing.unit,
+    flexGrow: 1
+  },
+  paper: {
+    height: 300,
+    marginBottom: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  message: {
+    padding: theme.spacing.unit,
+    display: 'flex'
+  },
+  avatar: {
     marginRight: theme.spacing.unit
   }
 })
@@ -23,11 +53,16 @@ class App extends Component {
     super()
 
     this.state = {
+      modalClosed: false,
+      forename: '',
+      surname: '',
       textfield: '',
-      messages: ''
+      messages: [],
+      users: []
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleModalSubmit = this.handleModalSubmit.bind(this)
   }
 
   componentDidMount () {
@@ -35,7 +70,25 @@ class App extends Component {
     this.getTest()
 
     socket.on('chat message', (message) => {
-      this.setState({messages: message})
+      const { messages } = this.state
+      messages.push(message)
+      this.setState({messages})
+    })
+
+    socket.on('new user', (user) => {
+      let { users } = this.state
+      const int = users.length
+      user.color = colors[int][500]
+      users.push(user)
+      this.setState({users})
+    })
+
+    socket.on('user disconnect', (disconnectedUserId) => {
+      let { users } = this.state
+      users = users.filter((user) => {
+        return user.id !== disconnectedUserId
+      })
+      this.setState({users})
     })
   }
 
@@ -69,12 +122,49 @@ class App extends Component {
     this.setState({textfield: ''})
   }
 
+  handleModalSubmit () {
+    const { forename, surname } = this.state
+
+    this.setState({modalClosed: true})
+    socket.emit('register', {forename, surname})
+  }
+
+  renderMessages () {
+    const { classes } = this.props
+
+    return this.state.messages.map((message, index) => {
+      const user = this.state.users.filter((user) => user.id === message.uid)[0]
+      let { initials, color } = ''
+
+      if (user) {
+        color = user.color
+        initials = `${user.forename.substr(0, 1)}${user.surname.substr(0, 1)}`
+      } else {
+        color = Grey[300]
+        initials = '?'
+      }
+
+      return (
+        <React.Fragment key={index}>
+          <div className={classes.message}>
+            <Avatar className={classes.avatar} style={{backgroundColor: color}}>{initials || '?'}</Avatar>
+            <span style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>{message.message}</span>
+          </div>
+          <Divider />
+        </React.Fragment>
+      )
+    })
+  }
+
   render () {
     const { classes } = this.props
 
     return (
       <div className={classes.root}>
-        <span>{this.state.messages}</span>
+        <Paper className={classes.paper}>
+          {this.renderMessages()}
+        </Paper>
+        <div className={classes.divider} />
         <div className={classes.textfieldContainer}>
           <TextField
             className={classes.textfield}
@@ -94,6 +184,32 @@ class App extends Component {
             Send
           </Button>
         </div>
+        <Dialog
+          open={!this.state.modalClosed}
+        >
+          <DialogTitle>What is your name?</DialogTitle>
+          <DialogContent>
+            <TextField
+              margin={'dense'}
+              label={'First Name'}
+              onChange={(e) => this.setState({forename: e.target.value})}
+              value={this.state.forename}
+              fullWidth
+            />
+            <TextField
+              margin={'dense'}
+              label={'Last Name'}
+              onChange={(e) => this.setState({surname: e.target.value})}
+              value={this.state.surname}
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleModalSubmit} color={'primary'}>
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     )
   }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const port = process.env.PORT || 8080
+let users = []
 
 server.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`))
 
@@ -24,13 +25,24 @@ app.post('/api/post', (req, res) => {
 io.on('connection', (connection) => {
   console.log('A user connected')
 
+  connection.on('register', (details) => {
+    const id = connection.id
+    const { forename, surname } = details
+    const user = {id, forename, surname}
+    users.push(user)
+    io.emit('new user', user)
+  })
+
   connection.on('chat', (message) => {
     console.log(`Chat message received from ${connection.id}: ${message}`)
-    io.emit('chat message', message)
+    const user = users.filter((user) => user.id === connection.id)[0]
+    io.emit('chat message', {uid: user.id, message})
   })
 
   connection.on('disconnect', () => {
     console.log(`Client ${connection.id} disconnected.`)
+    users = users.filter((user) => user.id !== connection.id)
+    io.emit('user disconnect', connection.id)
   })
 
   connection.on('error', (err) => {
